062fc7076a65d3a0109437f4e299431513a24e41
randomwalker2014 <randomwalker2014@gmail.com>
2019-09-15 19:19:22 -0400

    fixed variable parameter

--- minimal-notebook/jupyter_notebook_config.py
+++ minimal-notebook/jupyter_notebook_config.py
@@ -1,12 +1,11 @@
 import os
 
 def create_dir(subdirname):
-    print("called make dirs")
+    print("setting up user environment")
     username = c.Session.username
-    data_dir = os.path.join("/home",username,"-workspace",subdirname)
+    data_dir = os.path.join("/home",{username},"-workspace",subdirname)
     if not os.path.exists(data_dir):
         os.makedirs(data_dir,0o744)
-        print("called make dirs")
         
 port = int(os.environ.get('JUPYTER_NOTEBOOK_PORT', '8080'))
 
@@ -31,5 +30,8 @@ if os.path.exists(image_config_file):
     with open(image_config_file) as fp:
         exec(compile(fp.read(), image_config_file, 'exec'), globals())
         
-create_dir("my-junk")
+create_dir("my-datasets")
+create_dir("my-models")
+create_dir("my-notebooks")
+create_dir("my-results")
 
